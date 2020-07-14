8dd4b93442d70009841ce99f1e82dad755e4ca08
Christopher Coogan <c.coogan2201@gmail.com>
2020-07-14 09:49:43 -0400

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,18 +13,12 @@ git config --global user.email "$USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
 git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
 
-ls -la "$CLONE_DIR"
-
-# Copy files into the git and deletes all git
-# find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git" | grep -v "^$CLONE_DIR$" | xargs rm -rf # delete all files (to handle deletions)
-
-ls -la "$FOLDER"
-
 cp -r "$FOLDER" "$CLONE_DIR"
-ls -la "$CLONE_DIR"/"$FOLDER"
 
 cd "$CLONE_DIR"
 
 git add .
 git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA)"
+git status
 git push origin master
+git status
