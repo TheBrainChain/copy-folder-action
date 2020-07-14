a649ac3df6b8c9dc6ee483660ff653ba30aa7a59
Christopher Coogan <c.coogan2201@gmail.com>
2020-07-14 10:16:33 -0400

    add output folder selection

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
-echo "Starts"
-FOLDER="$1"
-GITHUB_USERNAME="$2"
-GITHUB_REPO="$3"
-USER_EMAIL="$4"
+SOURCE_FOLDER="$1"
+OUTPUT_FOLDER="$2"
+GITHUB_USERNAME="$3"
+GITHUB_REPO="$4"
+USER_EMAIL="$5"
 
 CLONE_DIR=$(mktemp -d)
 
@@ -13,12 +13,10 @@ git config --global user.email "$USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
 git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
 
-cp -r "$FOLDER" "$CLONE_DIR"
+cp -r "$SOURCE_FOLDER" "$CLONE_DIR"/"$OUTPUT_FOLDER"
 
 cd "$CLONE_DIR"
 
 git add .
 git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA)"
-git status
 git push origin master
-git status
